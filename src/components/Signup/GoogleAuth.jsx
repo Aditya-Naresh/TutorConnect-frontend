18b6ed134dc10043deed96f5466e64fd8e49312f
Aditya Naresh <adityanaresh007@gmail.com>
2024-10-09 22:23:10 +0530

    Google Auth set up Pending to fix the NEW user Forms and profile updations

---
+++ src/components/Signup/GoogleAuth.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, getAuth, GoogleAuthProvider} from 'firebase/auth'
+import {app} from '../utils/Firebase'
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {axiosPost} from "../../axios"
+import { loginSuccess } from "../../redux/slices/authSlice";
+import { toast } from "react-toastify";
+const GoogleAuth = () => {
+    const auth = getAuth(app)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider()
+    provider.setCustomParameters({prompt : "select_account"})
+    
+    try {
+        const resultsFromGoogle = await signInWithPopup(auth, provider)
+
+        const userData = {
+            email: resultsFromGoogle._tokenResponse.email,
+            first_name: resultsFromGoogle._tokenResponse.firstName,
+            last_name: resultsFromGoogle._tokenResponse.lastName
+        }
+
+        const response = await axiosPost('accounts/googleAuth/', userData)
+        console.log(response.status);
+        
+        if (response.status === 201 || response.status === 200) {
+            dispatch(loginSuccess(response.data))
+            toast.success(`${response.data.full_name} logged in`)
+            navigate('/')
+        }
+        
+        
+    } catch (error) {
+        console.log("err:",error);
+        
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <Button
+        onClick={handleGoogleSignIn}
+        variant="contained"
+        startIcon={<FcGoogle className="w-6 h-6" />}
+        className="!bg-white !text-gray-700 !font-semibold !hover:bg-gray-100 !border !border-gray-300 !shadow-md !py-2 !px-4 !rounded-lg !flex !items-center"
+      >
+        Sign in with Google
+      </Button>
+    </div>
+  );
+};
+
+export default GoogleAuth;
---
+++ src/components/utils/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfRaZRjt1vva4xWyjhhQ_tkexHTS-oiSg",
+  authDomain: "sample-firebase-ai-app-7e51b.firebaseapp.com",
+  projectId: "sample-firebase-ai-app-7e51b",
+  storageBucket: "sample-firebase-ai-app-7e51b.appspot.com",
+  messagingSenderId: "257429654999",
+  appId: "1:257429654999:web:4a7da384306d53e2444f92"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
